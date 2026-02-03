fd544a8312b88ac7cc72cce10b700e2cf5004da9
Ishika Chhajed <ishikachhajed68@gmail.com>
2026-02-03 16:06:39 +0530

    Added base url variable to axios

--- frontend/src/config/index.js
+++ frontend/src/config/index.js
@@ -3,6 +3,8 @@ import axios from "axios";
 // Read API base URL from environment (set on Vercel): NEXT_PUBLIC_API_URL
 // Fallback to the previous Render URL if not provided.
 
+const BASE_URL = process.env.BASE_URL;
+
 export const clientServer = axios.create({
   baseURL: process.env.BASE_URL,
 });
